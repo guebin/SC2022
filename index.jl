57a6a879df443314385be6b39976233d72e043eb
guebin <81734388+guebin@users.noreply.github.com>
2022-03-10 07:21:56 +0000

    Deploying to gh-pages from  @ aea352d42596b5c0d5410685f7b2efad94290e76 🚀

--- index.jl
+++ index.jl
@@ -9,6 +9,15 @@ md"""
 # 2022년 통계전산 강의홈페이지 
 """
 
+# ╔═╡ 2cfc8784-d7cf-4ab7-8934-855fecad2b26
+md"""
+### Contacts
+
+Email: guebin@jbnu.ac.kr 
+
+Kakao: [카카오톡채널](http://pf.kakao.com/_zNQxib/chat)
+"""
+
 # ╔═╡ f589bb03-2bf2-4f65-b351-0e3c53eae11b
 md"""
 ### Lectures
@@ -19,6 +28,8 @@ md"""
 [3월3일](https://guebin.github.io/SC2022/0303.html)
 
 [3월8일](https://guebin.github.io/SC2022/0308.html)
+
+[3월10일](https://guebin.github.io/SC2022/0310.html)
 """
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
@@ -38,6 +49,7 @@ manifest_format = "2.0"
 
 # ╔═╡ Cell order:
 # ╟─9ba3fd28-a02f-11ec-2d00-417ff6fef7d3
+# ╟─2cfc8784-d7cf-4ab7-8934-855fecad2b26
 # ╟─f589bb03-2bf2-4f65-b351-0e3c53eae11b
 # ╟─1f7b4001-be12-44dc-8745-c45ed7d2ad03
 # ╟─00000000-0000-0000-0000-000000000001
