67cc7b2d1a3e55577ed42ab1def779f4067073aa
guebin <81734388+guebin@users.noreply.github.com>
2022-03-10 05:11:25 +0000

    Deploying to gh-pages from  @ 8f51d3f68c267439d65d31211d39e01405729c99 🚀

---
+++ index.jl
@@ -0,0 +1,44 @@
+### A Pluto.jl notebook ###
+# v0.18.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 9ba3fd28-a02f-11ec-2d00-417ff6fef7d3
+md"""
+# 2022년 통계전산 강의홈페이지 
+"""
+
+# ╔═╡ f589bb03-2bf2-4f65-b351-0e3c53eae11b
+md"""
+### Lectures
+"""
+
+# ╔═╡ 1f7b4001-be12-44dc-8745-c45ed7d2ad03
+md"""
+[3월3일](https://guebin.github.io/SC2022/0303.html)
+
+[3월8일](https://guebin.github.io/SC2022/0308.html)
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─9ba3fd28-a02f-11ec-2d00-417ff6fef7d3
+# ╟─f589bb03-2bf2-4f65-b351-0e3c53eae11b
+# ╟─1f7b4001-be12-44dc-8745-c45ed7d2ad03
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
