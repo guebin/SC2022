2de0d940475d8de8329feb5b9363de483396d8b3
cgb2 <miruetoto@gmail.com>
2022-03-10 13:56:42 +0900

    .

---
+++ 0303.jl
@@ -0,0 +1,36 @@
+### A Pluto.jl notebook ###
+# v0.18.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 13159864-a02e-11ec-226a-3f0a35ca113d
+md"""
+# 3월3일 강의영상 
+"""
+
+# ╔═╡ 32d19393-384d-4ed9-a09c-c27ba5dfd358
+html"""
+<div notthestyle="position: relative; right: 0; top: 0; z-index: 300;"><iframe src="https://www.youtube.com/embed/playlist?list=PLQqh36zP38-zXxHUBMMx6eiiqLu-Ehvpy" width=600 height=375  frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═13159864-a02e-11ec-226a-3f0a35ca113d
+# ╠═32d19393-384d-4ed9-a09c-c27ba5dfd358
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
