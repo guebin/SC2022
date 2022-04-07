92247d2c891eafc47d2985c466b27fcbfb2c1d4b
miruetoto <44726070+miruetoto@users.noreply.github.com>
2022-04-07 06:53:09 +0000

    Deploying to gh-pages from  @ e61965283cfa1506ffcbb01ca2fc5db538c0cc3f 🚀

---
+++ .ipynb_checkpoints/index-checkpoint.jl
@@ -0,0 +1,82 @@
+### A Pluto.jl notebook ###
+# v0.18.2
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 9ba3fd28-a02f-11ec-2d00-417ff6fef7d3
+md"""
+# 2022년 통계전산 강의홈페이지 
+"""
+
+# ╔═╡ 2cfc8784-d7cf-4ab7-8934-855fecad2b26
+md"""
+### Contacts
+
+- Email: guebin@jbnu.ac.kr 
+- Kakao: [카카오톡채널](http://pf.kakao.com/_zNQxib/chat)
+"""
+
+# ╔═╡ f589bb03-2bf2-4f65-b351-0e3c53eae11b
+md"""
+### Lectures
+"""
+
+# ╔═╡ 1f7b4001-be12-44dc-8745-c45ed7d2ad03
+md"""
+[3월3일](https://guebin.github.io/SC2022/0303.html)
+- 강의소개 
+
+[3월8일](https://guebin.github.io/SC2022/0308.html)
+- 인터렉티브 노트북, 플루토의 단점, 플루토에서 여러줄의 코드를 작성하는 방법, 플루투의 마크다운이 주피터보다 좋은점, 위젯사용법(PlutoUI)
+
+[3월10일](https://guebin.github.io/SC2022/0310.html)
+- 변수이름, 자료형 (int,float,Inf,NaN,문자열,튜플,array)
+
+[3월15일](https://guebin.github.io/SC2022/0315.html)
+- 자료형 (matrix: 선언, 연산), 벡터와 매트릭스 자투리(range, index), 함수선언 
+
+[3월17일](https://guebin.github.io/SC2022/0317.html)
+- 3월15일 숙제풀이, 함수고급(연산자는 사실 함수였음, 브로드캐스팅, 파이프연산자, 피보나치수열) 
+
+[3월22일](https://guebin.github.io/SC2022/0322.html)
+- 베르누이, 이항분포 
+
+[3월24일](https://guebin.github.io/SC2022/0324.html)
+- 포아송분포, 지수분포 
+
+[3월29일](https://guebin.github.io/SC2022/0329.html)
+- 지수분포 (inverse cdf, 박스뮬러 알고리즘)
+
+[3월31일](https://guebin.github.io/SC2022/0331.html)
+- inverse cdf의 이론적 근거, 어느 사격수의 이야기 
+
+[4월5일](https://guebin.github.io/SC2022/0405.html)
+- 어느 사격수의 이야기2, 정규분포 (기본요약, 신뢰구간)
+
+[4월7일](https://guebin.github.io/SC2022/0407.html)
+- 정규분포 (CLT, 가설검정)
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─9ba3fd28-a02f-11ec-2d00-417ff6fef7d3
+# ╟─2cfc8784-d7cf-4ab7-8934-855fecad2b26
+# ╟─f589bb03-2bf2-4f65-b351-0e3c53eae11b
+# ╟─1f7b4001-be12-44dc-8745-c45ed7d2ad03
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
