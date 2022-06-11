24543ea706ae618679d2cd9b212529113b50514b
miruetoto <44726070+miruetoto@users.noreply.github.com>
2022-06-11 08:54:07 +0000

    Deploying to gh-pages from  @ 025cc085569bda790102d5a4135bd05f19a2ec76 🚀

--- index.jl
+++ index.jl
@@ -23,6 +23,8 @@ md"""
 - [시험관련 안내사항](https://guebin.github.io/SC2022/notice1.html)
 - [중간고사 예상유형](https://guebin.github.io/SC2022/mid_prac.html)
 - [중간고사](https://guebin.github.io/SC2022/mid.html) -> [해설강의](https://guebin.github.io/SC2022/0428.html)
+- [기말고사 예상유형](https://guebin.github.io/SC2022/final_prac.html)
+- [기말고사](https://guebin.github.io/SC2022/final.html) -> [해설강의]()
 """
 
 # ╔═╡ f589bb03-2bf2-4f65-b351-0e3c53eae11b
