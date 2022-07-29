2365bd0f352baf9cc19279c0ed5a362c180f65c0
cgb2 <guebin@jbnu.ac.kr>
2022-07-30 06:10:02 +0900

    .

---
+++ start.jl
@@ -0,0 +1,29 @@
+### A Pluto.jl notebook ###
+# v0.19.9
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ d83c4152-0f81-11ed-3041-11d8f8547a3d
+import PlutoSliderServer
+PlutoSliderServer.run_directory("."; static_export=true,config_toml_path="./PlutoDeployment.toml")
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═d83c4152-0f81-11ed-3041-11d8f8547a3d
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
