191e8dcb09f989dec1805133e9c8bd21bb237726
miruetoto <guebin@jbnu.ac.kr>
2024-05-03 19:45:45 +0900

    .

--- 0531.jl
+++ 0531.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.4
+# v0.19.41
 
 using Markdown
 using InteractiveUtils
@@ -116,6 +116,15 @@ md"""
 ## Positive definite matrix
 """
 
+# ╔═╡ 1309cb46-7027-45b4-9401-0ed38a1ef7a7
+md"""
+	이 강의노트의 PD-matrix는 일부문헌의 정의를 사용하여 설명한 것입니다. (예시는 아래..)
+
+	- Golub, G. H., & Van Loan, C. (1979). Unsymmetric positive definite linear systems. Linear Algebra and its Applications, 28, 85-97.
+
+	현재는 PD-matrix가 실대칭행렬(or 에르미트행렬)임을 전재하고 서술한 문현이 많으므로 해당내용으로 공부하시기 바랍니다. 2024년 강의노트 버전 <https://github.com/guebin/SC2024> 에서는 다른 정의로 PD를 설명합니다. 
+"""
+
 # ╔═╡ c226e8e1-e97c-4039-8baa-f82a00254917
 md"""
 ### Positive definite matrix 정의 
@@ -123,7 +132,7 @@ md"""
 
 # ╔═╡ 031acc24-0027-4db0-9270-b63a026e9575
 md"""
-`-` 정의: 어떠한 매트릭스 ${\bf A}_{n\times n}$가 모든 non-zero vector ${\bf y}_{n \times 1}$에 대하여 
+`-` 정의: 어떠한 실대칭행렬 ${\bf A}_{n\times n}$가 모든 non-zero real vector ${\bf y}_{n \times 1}$에 대하여 
 
 $${\bf y}^\top {\bf A}{\bf y} > 0$$
 
@@ -262,8 +271,9 @@ PlutoUI = "~0.7.39"
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.7.2"
+julia_version = "1.8.5"
 manifest_format = "2.0"
+project_hash = "1fb51a3a2be50adf36eba7104e57e2aef7a5de82"
 
 [[deps.AbstractPlutoDingetjes]]
 deps = ["Pkg"]
@@ -273,6 +283,7 @@ version = "1.1.4"
 
 [[deps.ArgTools]]
 uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
+version = "1.1.1"
 
 [[deps.Artifacts]]
 uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
@@ -289,14 +300,19 @@ version = "0.11.3"
 [[deps.CompilerSupportLibraries_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "e66e0078-7015-5450-92f7-15fbd957f2ae"
+version = "1.0.1+0"
 
 [[deps.Dates]]
 deps = ["Printf"]
 uuid = "ade2ca70-3891-5945-98fb-dc099432e06a"
 
 [[deps.Downloads]]
-deps = ["ArgTools", "LibCURL", "NetworkOptions"]
+deps = ["ArgTools", "FileWatching", "LibCURL", "NetworkOptions"]
 uuid = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
+version = "1.6.0"
+
+[[deps.FileWatching]]
+uuid = "7b1f6079-737a-58dc-b8bc-7a2ca5c1b5ee"
 
 [[deps.FixedPointNumbers]]
 deps = ["Statistics"]
@@ -335,10 +351,12 @@ version = "0.21.3"
 [[deps.LibCURL]]
 deps = ["LibCURL_jll", "MozillaCACerts_jll"]
 uuid = "b27032c2-a3e7-50c8-80cd-2d36dbcbfd21"
+version = "0.6.3"
 
 [[deps.LibCURL_jll]]
 deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll", "Zlib_jll", "nghttp2_jll"]
 uuid = "deac9b47-8bc7-5906-a0fe-35ac56dc84c0"
+version = "7.84.0+0"
 
 [[deps.LibGit2]]
 deps = ["Base64", "NetworkOptions", "Printf", "SHA"]
@@ -347,6 +365,7 @@ uuid = "76f85450-5226-5b5a-8eaa-529ad045b433"
 [[deps.LibSSH2_jll]]
 deps = ["Artifacts", "Libdl", "MbedTLS_jll"]
 uuid = "29816b5a-b9ab-546f-933c-edad1886dfa8"
+version = "1.10.2+0"
 
 [[deps.Libdl]]
 uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
@@ -365,19 +384,23 @@ uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
 [[deps.MbedTLS_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
+version = "2.28.0+0"
 
 [[deps.Mmap]]
 uuid = "a63ad114-7e13-5084-954f-fe012c677804"
 
 [[deps.MozillaCACerts_jll]]
 uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
+version = "2022.2.1"
 
 [[deps.NetworkOptions]]
 uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
+version = "1.2.0"
 
 [[deps.OpenBLAS_jll]]
 deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
 uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
+version = "0.3.20+0"
 
 [[deps.Parsers]]
 deps = ["Dates"]
@@ -388,6 +411,7 @@ version = "2.3.1"
 [[deps.Pkg]]
 deps = ["Artifacts", "Dates", "Downloads", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
 uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
+version = "1.8.0"
 
 [[deps.PlutoUI]]
 deps = ["AbstractPlutoDingetjes", "Base64", "ColorTypes", "Dates", "Hyperscript", "HypertextLiteral", "IOCapture", "InteractiveUtils", "JSON", "Logging", "Markdown", "Random", "Reexport", "UUIDs"]
@@ -414,6 +438,7 @@ version = "1.2.2"
 
 [[deps.SHA]]
 uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
+version = "0.7.0"
 
 [[deps.Serialization]]
 uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
@@ -432,10 +457,12 @@ uuid = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
 [[deps.TOML]]
 deps = ["Dates"]
 uuid = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
+version = "1.0.0"
 
 [[deps.Tar]]
 deps = ["ArgTools", "SHA"]
 uuid = "a4e569a6-e804-4fa4-b0f3-eef7a1d5b13e"
+version = "1.10.1"
 
 [[deps.Test]]
 deps = ["InteractiveUtils", "Logging", "Random", "Serialization"]
@@ -456,18 +483,22 @@ uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
 [[deps.Zlib_jll]]
 deps = ["Libdl"]
 uuid = "83775a58-1f1d-513f-b197-d71354ab007a"
+version = "1.2.12+3"
 
 [[deps.libblastrampoline_jll]]
 deps = ["Artifacts", "Libdl", "OpenBLAS_jll"]
 uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
+version = "5.1.1+0"
 
 [[deps.nghttp2_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "8e850ede-7688-5339-a07c-302acd2aaf8d"
+version = "1.48.0+0"
 
 [[deps.p7zip_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
+version = "17.4.0+0"
 """
 
 # ╔═╡ Cell order:
@@ -487,9 +518,10 @@ uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
 # ╟─05af5915-30a6-49fd-850a-352a41036984
 # ╠═9022b592-6ab0-40de-b96b-aecb56c663a5
 # ╟─f080d7b0-4c1f-411e-9e86-d422dd77d2e7
+# ╟─1309cb46-7027-45b4-9401-0ed38a1ef7a7
 # ╟─c226e8e1-e97c-4039-8baa-f82a00254917
 # ╟─031acc24-0027-4db0-9270-b63a026e9575
-# ╟─eac996bd-5458-40eb-a82d-88f3aac35033
+# ╠═eac996bd-5458-40eb-a82d-88f3aac35033
 # ╠═4d737613-177c-45e8-abe7-597ffd863049
 # ╠═df004a39-17f0-47eb-9725-d46dd844f205
 # ╠═4eabb492-2f64-4eb1-9041-a8d29eb40ff8
